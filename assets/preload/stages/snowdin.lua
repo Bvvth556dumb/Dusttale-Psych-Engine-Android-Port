c6b9e02c1cac7412b5fd7ab705eb1e48e3553514
Bvvth556dumb <96244726+Bvvth556dumb@users.noreply.github.com>
2021-12-17 06:22:28 +0700

    Add files via upload

---
+++ assets/preload/stages/snowdin.lua
@@ -0,0 +1,19 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('snowdin', 'snowdin', -616, -186);
+	addLuaSprite('snowdin', false);
+	close(true)
+
+end
+
+function onBeatHit()
+
+end
+
+function onStepHit()
+
+end
+
+function onUpdate()
+
+end
