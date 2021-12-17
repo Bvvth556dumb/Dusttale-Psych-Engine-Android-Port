d41bb1558fccccee3e06193ba6d28e90cf0bc414
Bvvth556dumb <96244726+Bvvth556dumb@users.noreply.github.com>
2021-12-17 11:16:15 +0700

    Add files via upload

---
+++ assets/preload/stages/waterfall.lua
@@ -0,0 +1,19 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('waterfall', 'waterfall', -616, -186);
+	addLuaSprite('waterfall', false);
+	close(true)
+
+end
+
+function onBeatHit()
+
+end
+
+function onStepHit()
+
+end
+
+function onUpdate()
+
+end
